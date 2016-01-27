c6d2f93c79a4ef5da5e0bc176b8a8f5ca1526484
Guilherme Emmanuel <guilherme1.0emmanuel@gmail.com>
2016-01-26 23:57:26 -0200

    Bubble, Ring and Platform

---
+++ Assets/Scripts/BubbleControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleControl : MonoBehaviour {
+
+
+	public int speed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector2(0, 1) * Time.deltaTime * speed);
+	}
+}
