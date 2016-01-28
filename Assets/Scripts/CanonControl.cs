4dbf9229a4ff8b6471009cc1705e52d256a0fd71
Guilherme Emmanuel <guilherme1.0emmanuel@gmail.com>
2016-01-28 00:07:13 -0200

    Canon and Ground

---
+++ Assets/Scripts/CanonControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonControl : MonoBehaviour {
+
+	public GameObject bubblePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SpawnBubble() {
+		Instantiate (bubblePrefab, transform.position + new Vector3(0,1,0), Quaternion.identity);
+	}
+
+}
---
+++ Assets/Scripts/InputControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputControl : MonoBehaviour {
+
+	public CanonControl spawnRight;
+	public CanonControl spawnLeft;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("s")) {
+			spawnLeft.SpawnBubble ();
+		} else if (Input.GetKeyDown ("d")) {
+			spawnRight.SpawnBubble ();
+		}
+	}
+}
