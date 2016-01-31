80546aa16e18e18abf988d3b90fa3d401e34099f
Guilherme Emmanuel <guilherme1.0emmanuel@gmail.com>
2016-01-31 00:57:55 -0200

    Score

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+	public UnityEngine.UI.Text scoreText;
+
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+		scoreText.text = "Score: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addScore(int addScore) {
+		score += addScore;
+		scoreText.text = "Score: " + score;
+	}
+
+}
---
+++ Assets/Scripts/RingControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RingControl : MonoBehaviour {
+
+	public GameControl game;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "StickCenter") {
+			game.addScore (10);
+		}
+	}
+
+}
