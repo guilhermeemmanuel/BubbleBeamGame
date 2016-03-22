7190fdff18a3fd9675001ed3b2a9766727afb037
Guilherme Emmanuel <guilherme1.0emmanuel@gmail.com>
2016-03-21 23:30:24 -0300

    Input

--- Assets/Scripts/InputControl.cs
+++ Assets/Scripts/InputControl.cs
@@ -14,6 +14,24 @@ public class InputControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		
+
+		if (Input.GetButtonDown ("Fire1")) {
+			float mousex = (Input.mousePosition.x);
+			float mousey = (Input.mousePosition.y);
+			Vector2 mouseposition = Camera.main.ScreenToWorldPoint(new Vector2 (mousex,mousey));
+
+			if (mouseposition.x < 0) {
+				spawnLeft.SpawnBubble ();
+			} else {
+				spawnRight.SpawnBubble ();
+			}
+
+
+
+		}
+
+
 		if (Input.GetKeyDown ("s")) {
 			spawnLeft.SpawnBubble ();
 		} else if (Input.GetKeyDown ("d")) {
--- Assets/Scripts/RingControl.cs
+++ Assets/Scripts/RingControl.cs
@@ -5,14 +5,18 @@ public class RingControl : MonoBehaviour {
 
 	public GameControl game;
 
+	Rigidbody2D body;
+
 	// Use this for initialization
 	void Start () {
-	
+		body = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		body.AddForce (new Vector2 (0, Input.acceleration.x));
+
+
 	}
 
 	public void OnTriggerEnter2D (Collider2D col) {
